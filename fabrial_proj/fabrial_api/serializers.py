1fd4ea0e8bba8c9b7d38782aaec50f46c884586f
matir11 <hollisterdd@hotmail.com>
2022-06-10 00:29:55 -0400

    no idea if the JS is here

--- fabrial_proj/fabrial_api/serializers.py
+++ fabrial_proj/fabrial_api/serializers.py
@@ -1,22 +1,36 @@
-from pkg_resources import require
+# from pkg_resources import require
+# from rest_framework import serializers
+# from .models import Chore
+# from django.contrib.auth.models import User
+# from rest_framework.authtoken.models import Token
+
+# class ChoreSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Chore
+#         fields = '__all__'
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('id','username', 'password')
+#         extra_kwargs = {'password': {'required': True, 'write_only':True}}
+
+#         def create(self, validated_data):
+#             user = User.objects.create_user(**validated_data)
+#             Token.objects.create(user=user)
+#             return user
+
+from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import Chore
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
 
-class ChoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Chore
-        fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id','username', 'password')
-        extra_kwargs = {'password': {'required': True, 'write_only':True}}
+        fields = ['url', 'username', 'email', 'groups']
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            Token.objects.create(user=user)
-            return user
 
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
--- fabrial_proj/fabrial_api/views.py
+++ fabrial_proj/fabrial_api/views.py
@@ -1,22 +1,44 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from rest_framework import viewsets
+# from .models import Chore
+# from .serializers import ChoreSerializer, UserSerializer
+# from rest_framework.permissions import IsAuthenticated, AllowAny
+# from django.contrib.auth.models import User
+# from rest_framework.authentication import TokenAuthentication
+# from rest_framework import permissions
+
+
+# class ChoreViewSet(viewsets.ModelViewSet):
+#     queryset=Chore.objects.all()
+#     serializer_class=ChoreSerializer
+#     authentication_classes = (TokenAuthentication,)
+#     permission_classes= (permissions.IsAuthenticated,)
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     permission_classes = (permissions.IsAuthenticated,) 
+
+# # Create your views here.
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .models import Chore
-from .serializers import ChoreSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from django.contrib.auth.models import User
-from rest_framework.authentication import TokenAuthentication
 from rest_framework import permissions
+from .serializers import UserSerializer, GroupSerializer
 
-
-class ChoreViewSet(viewsets.ModelViewSet):
-    queryset=Chore.objects.all()
-    serializer_class=ChoreSerializer
-    authentication_classes = (TokenAuthentication,)
-    permission_classes= (permissions.IsAuthenticated,)
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated,) 
+    permission_classes = [permissions.IsAuthenticated]
 
-# Create your views here.
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
